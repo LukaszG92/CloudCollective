568beb95f8c0406d5dec123417dc81cb90dff25d
LukaszGajewski8 <114994048+LukaszGajewski8@users.noreply.github.com>
2024-02-04 12:08:09 +0100

    Added ssl to sequelize constructor

--- src/utils/database.js
+++ src/utils/database.js
@@ -3,7 +3,8 @@ require('dotenv').config()
 
 const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
-  dialect: 'mysql'
+  dialect: 'mysql',
+  ssl: true
 });
 
 module.exports = sequelize;
