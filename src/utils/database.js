caad0abdde6caa53f154f10ae938b8279caf9041
LukaszGajewski8 <114994048+LukaszGajewski8@users.noreply.github.com>
2024-02-04 16:29:32 +0100

    Changed path to SLL certificate

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,12 +1,13 @@
 const Sequelize = require('sequelize');
 const fs = require("fs");
+const path = require("path");
 require('dotenv').config()
 
 const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
   ssl: {
-    cert: fs.readFileSync("../DigiCertGlobalRootG2.crt.pem")
+    cert: fs.readFileSync(path.resolve(__dirname, 'src', 'DigiCertGlobalRootG2.crt.pem'))
   }
 });
 
