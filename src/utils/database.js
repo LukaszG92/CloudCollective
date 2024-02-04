08ec7115b86178d919a1ac90a7129854b8c87fe5
LukaszGajewski8 <114994048+LukaszGajewski8@users.noreply.github.com>
2024-02-04 15:31:43 +0100

    Added SSL certificate

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,10 +1,13 @@
 const Sequelize = require('sequelize');
+const fs = require("fs");
 require('dotenv').config()
 
 const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
-  ssl: true
+  ssl: {
+    cert: fs.readFileSync("../DigiCertGlobalRootG2.crt.pem")
+  }
 });
 
 module.exports = sequelize;
