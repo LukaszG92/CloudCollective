7452825b20cc130e0ed1fabb3dcad17779d18309
LukaszGajewski8 <114994048+LukaszGajewski8@users.noreply.github.com>
2024-02-04 20:54:53 +0100

    Changed SSL configuration

--- src/utils/database.js
+++ src/utils/database.js
@@ -6,8 +6,12 @@ require('dotenv').config()
 const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
-  ssl: {
-    cert: fs.readFileSync(path.resolve(__dirname, '..', 'DigiCertGlobalRootG2.crt.pem')).toString()
+  ssl: true,
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: true,
+      ca: fs.readFileSync(path.resolve(__dirname, '..', 'DigiCertGlobalRootG2.crt.pem')).toString()
+    }
   }
 });
 
